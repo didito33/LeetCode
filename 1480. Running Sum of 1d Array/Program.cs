a76a88865241ce562b89ff8f3c1a45ebbdcc71d7
didito33 <didito331324@gmail.com>
2023-07-11 15:14:30 +0300

    Day 1

---
+++ 1480. Running Sum of 1d Array/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+Console.WriteLine(string.Join(", ", RunningSum(nums)));
+
+int[] RunningSum(int[] nums)
+{
+    int[] result = new int[nums.Length];
+    for (int i = 0; i < nums.Length; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            result[i] += nums[j];
+        }
+    }
+    return result;
+}
---
+++ 724. Find Pivot Index/Program.cs
@@ -0,0 +1,20 @@
+﻿int[] array = { 1, 7, 3, 6, 5, 6 };
+Console.WriteLine(PivotIndex(array));
+
+int PivotIndex(int[] nums)
+{
+    int leftSum = 0;
+    int rightSum = nums.Sum();
+    for (int i = 0; i < nums.Length; i++)
+    {
+        rightSum -= nums[i];
+
+        if (leftSum == rightSum)
+        {
+            return i;
+        }
+
+        leftSum += nums[i];
+    }
+    return -1;
+}
