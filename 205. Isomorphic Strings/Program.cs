d90448a910f1efdf96f1a067836b45b024c159ee
didito33 <didito331324@gmail.com>
2023-07-12 13:27:23 +0300

    Day 2

---
+++ 205. Isomorphic Strings/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine(IsIsomorphic("blabla", "gbagba"));
+
+bool IsIsomorphic(string s, string t) 
+{
+    var sDict = new Dictionary<char, char>();
+    var tDict = new Dictionary<char, char>();
+    for (int i = 0; i < s.Length; i++)
+    {
+        if (!sDict.ContainsKey(s[i]))
+        {
+            sDict.Add(s[i], t[i]);
+        }
+        if (!tDict.ContainsKey(t[i]))
+        {
+            tDict.Add(t[i], s[i]);
+        }
+        if (sDict[s[i]] != t[i] || tDict[t[i]] != s[i])
+            return false;
+
+    }
+   
+    return true;
+}
---
+++ 392. Is Subsequence/Program.cs
@@ -0,0 +1,44 @@
+﻿Console.WriteLine(IsSubsequence("b","abc"));//abc   plllabc
+
+bool IsSubsequence(string s, string t)
+{
+    if (s == String.Empty) return true;
+    if (t== String.Empty) return false;
+
+    char[] sChar = s.ToCharArray();
+    char[] tChar = t.ToCharArray();  
+    int count = 0;
+    bool answer = false;
+
+    if (sChar.Length <= tChar.Length)
+    {
+        for (int i = 0; i < tChar.Length; i++)
+        {
+            if (sChar.Length == count)
+            {
+                answer = true;
+                break;
+            }
+
+            if (sChar[count] == tChar[i]) count++;
+        }
+        answer = count == sChar.Length ? true : false;
+        
+    }
+
+    else if(sChar.Length > tChar.Length)
+    {
+        for (int i = 0; i < sChar.Length; i++)
+        {
+            if (tChar.Length == count)
+            {
+                answer = true;
+                break;
+            }
+            if (tChar[count] == sChar[i]) count++;
+        }
+        answer = count == tChar.Length ? true : false;
+    }
+
+    return answer;
+}
