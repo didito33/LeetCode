d0392e55d9a1344e2c92f0f6d3668c2b2cefb6eb
didito33 <didito331324@gmail.com>
2023-07-13 11:44:50 +0300

    Day 3

---
+++ 21. Merge Two Sorted Lists/Program.cs
@@ -0,0 +1,9 @@
+﻿List<int> MergeTwoLists(List<int> list1, List<int> list2)
+{
+   List<int> result = new List<int>(list1.Count + list2.Count);
+    result.AddRange(list1);
+    result.AddRange(list2);
+    result.Sort();
+    return result;
+}
+
