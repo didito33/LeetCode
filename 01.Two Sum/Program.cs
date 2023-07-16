1d70c80c8f63f384ebfce4ae08fb57508546c404
didito33 <didito331324@gmail.com>
2023-07-16 18:11:11 +0300

    Day 4

---
+++ 01.Two Sum/Program.cs
@@ -0,0 +1,19 @@
+﻿int[] array = { 3,2,4 };
+Console.WriteLine($"[{String.Join(",",TwoSum(array,6))}]");
+int[] TwoSum(int[] nums, int target)
+{
+    int[] indexes = new int[2];
+    for (int i = 0; i < nums.Length; i++)
+    {
+        for (int j = i+1; j < nums.Length; j++)
+        {
+            if (nums[i] + nums[j] == target)
+            {
+                indexes[0] = i > j ? j : i;
+                indexes[1] = i > j ? i : j;
+                return indexes;
+            }
+        }
+    }
+    return new int[] {} ;
+}
---
+++ 03. Longest Substring Without Repeating Characters/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine(LengthOfLongestSubstring("pwwkew"));
+int LengthOfLongestSubstring(string s)
+{
+	var charSet = new HashSet<char>();
+	int left = 0, right = 0, maxlength = 0;
+	while(right < s.Length)
+	{
+		if (!charSet.Contains(s[right]))
+		{
+			charSet.Add(s[right]);
+			right++;
+			maxlength = Math.Max(maxlength, charSet.Count);
+		}
+		else
+		{
+			charSet.Remove(s[left]);
+			left++;
+		}
+	}
+	//Console.WriteLine(String.Join(",",charSet));
+	return maxlength;
+}
